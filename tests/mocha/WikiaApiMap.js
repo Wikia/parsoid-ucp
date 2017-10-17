e68424a979f2640c8d8b2865597650ab92c58dc7
TK-999 <tk.999.wikia@gmail.com>
2017-10-18 00:35:13 +0200

    write tests
    Change-Id: Iddc21a16e1ee5313704d57a5d946b698f6a39708

---
+++ tests/mocha/WikiaApiMap.js
@@ -0,0 +1,31 @@
+/* global describe, it */
+
+'use strict';
+
+require('chai').should();
+
+var WikiaApiMap = require('../../lib/config/WikiaApiMap');
+
+describe('WikiaApiMap', function () {
+	var wikiaApiMap;
+
+	beforeEach(function () {
+		wikiaApiMap = new WikiaApiMap();
+	});
+
+	it('returns stored value if key exists in underlying map', function () {
+		wikiaApiMap.apiMap.set('foo', 'bar');
+
+		wikiaApiMap.get('foo').should.equal('bar');
+	});
+
+	it('returns configuration if key does not exist but is valid URL', function () {
+		var testCases = [];
+
+		testCases.forEach(function (url) {
+			var config = wikiaApiMap.get(url);
+
+			config.should.be
+		});
+	});
+});
